8d721e30536c3968a16760879d40a5c9e0271393
Tzurrr <zur001@gmail.com>
2022-01-17 15:11:57 +0200

    added a reference to the writer class

--- src/main/java/TheConstantsSampler/LabTests/LabTestsMain.java
+++ src/main/java/TheConstantsSampler/LabTests/LabTestsMain.java
@@ -1,24 +1,24 @@
 package TheConstantsSampler.LabTests;
 
+import TheConstantsSampler.Enrichment.CSVLabtestEnricher;
 import TheConstantsSampler.Readers.CSVReaderForLabtests;
+import TheConstantsSampler.Writers.XMLWriter;
+import health_care_provider.errors.InvalidIdException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class LabTestsMain {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InvalidIdException {
         CSVReaderForLabtests a = new CSVReaderForLabtests("D:\\Code\\TheConstantSampler\\src\\main\\resources\\LabTests.csv");
+        CSVLabtestEnricher b = new CSVLabtestEnricher();
+        ArrayList<TestData> tests_enriched = new ArrayList<TestData>();
+        XMLWriter c = new XMLWriter();
+
         for (int i = 0; i < a.readLabtest().size(); i++) {
-            System.out.println(a.readLabtest().get(i).getIDNum());
-            System.out.println(a.readLabtest().get(i).getIDType());
-            System.out.println(a.readLabtest().get(i).getFirstName());
-            System.out.println(a.readLabtest().get(i).getLastName());
-            System.out.println(a.readLabtest().get(i).getResultDate());
-            System.out.println(a.readLabtest().get(i).getBirthDate());
-            System.out.println(a.readLabtest().get(i).getLabcode());
-            System.out.println(a.readLabtest().get(i).getStickerNumber());
-            System.out.println(a.readLabtest().get(i).getResultTestCorona());
-            System.out.println(a.readLabtest().get(i).getVariant());
-            System.out.println(a.readLabtest().get(i).getTestType());
+            TestData temp = new TestData(a.readLabtest().get(i).getIDNum(), a.readLabtest().get(i).getIDType(), a.readLabtest().get(i).getFirstName(), a.readLabtest().get(i).getLastName(), a.readLabtest().get(i).getResultDate(), a.readLabtest().get(i).getBirthDate(), a.readLabtest().get(i).getLabcode(), a.readLabtest().get(i).getStickerNumber(), a.readLabtest().get(i).getResultTestCorona(), a.readLabtest().get(i).getVariant(), a.readLabtest().get(i).getTestType(), b.enrich(a.readLabtest().get(i).getIDNum(), a.readLabtest().get(i).getIDType()).getJoinDate(), b.enrich(a.readLabtest().get(i).getIDNum(), a.readLabtest().get(i).getIDType()).getHealthCareId(), b.enrich(a.readLabtest().get(i).getIDNum(), a.readLabtest().get(i).getIDType()).getHealthCareName());
+            tests_enriched.add(temp);
         }
+        c.write(tests_enriched);
     }
 }
