34628e3f4c287512d1ac9efd935629a59e34b3d0
Tzurrr <zur001@gmail.com>
2022-01-17 09:24:22 +0200

    checked if the input's fine

--- src/main/java/TheConstantsSampler/LabTests/LabTestsMain.java
+++ src/main/java/TheConstantsSampler/LabTests/LabTestsMain.java
@@ -1,4 +1,24 @@
 package TheConstantsSampler.LabTests;
 
+import TheConstantsSampler.Readers.CSVReaderForLabtests;
+
+import java.io.IOException;
+
 public class LabTestsMain {
+    public static void main(String[] args) throws IOException{
+        CSVReaderForLabtests a = new CSVReaderForLabtests("D:\\Code\\TheConstantSampler\\src\\main\\resources\\LabTests.csv");
+        for (int i = 0; i < a.readLabtest().size(); i++) {
+            System.out.println(a.readLabtest().get(i).getIDNum());
+            System.out.println(a.readLabtest().get(i).getIDType());
+            System.out.println(a.readLabtest().get(i).getFirstName());
+            System.out.println(a.readLabtest().get(i).getLastName());
+            System.out.println(a.readLabtest().get(i).getResultDate());
+            System.out.println(a.readLabtest().get(i).getBirthDate());
+            System.out.println(a.readLabtest().get(i).getLabcode());
+            System.out.println(a.readLabtest().get(i).getStickerNumber());
+            System.out.println(a.readLabtest().get(i).getResultTestCorona());
+            System.out.println(a.readLabtest().get(i).getVariant());
+            System.out.println(a.readLabtest().get(i).getTestType());
+        }
+    }
 }
