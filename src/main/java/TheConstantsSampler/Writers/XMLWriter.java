22580a8f091c796dd88236a61744a27794761cef
Tzurrr <zur001@gmail.com>
2022-01-18 18:13:30 +0200

    changed the way the class writes

--- src/main/java/TheConstantsSampler/Writers/XMLWriter.java
+++ src/main/java/TheConstantsSampler/Writers/XMLWriter.java
@@ -4,11 +4,14 @@ import TheConstantsSampler.LabTests.TestData;
 import TheConstantsSampler.Monitors.CSVMonitor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class XMLWriter implements Writer{
     XmlMapper xmlMapper = new XmlMapper();
@@ -18,35 +21,39 @@ public class XMLWriter implements Writer{
     private File path = new File("D:\\Code\\TheConstantSampler\\src\\main\\resources\\LABTESTS\\labtest_n" + this.counter + ".XML");
 
 
-    public void write(ArrayList<TestData> labTest) throws IOException {
+    public void write(List<TestData> testsData) throws IOException {
         xmlMapper.findAndRegisterModules();
         int inner_counter = 0;
-        TestData[] temp = new TestData[labTest.size()];
+        TestData[] temp = new TestData[testsData.size()];
 
-        if(labTest.size() > 50000){
-            int split_to = mon.split(labTest.size());
+        if(testsData.size() > 50000){
+            int split_to = mon.split(testsData.size());
             //ArrayList<TestData> temp = new ArrayList<TestData>();
 
             for (int i = 0; i <split_to; i++) {
 
                 while (inner_counter < 50000){
-                    temp[inner_counter] = labTest.get(0);
+                    //temp[inner_counter] = testsData.get(0);
                     inner_counter++;
-                    labTest.remove(0);
+                    testsData.remove(0);
                 }
                 xmlMapper.writeValue(this.path, temp);
                 this.counter++;
                 this.path = new File("D:\\Code\\TheConstantSampler\\src\\main\\resources\\LABTESTS\\labtest_n" + this.counter + ".XML");
                 inner_counter = 0;
-                temp = new TestData[labTest.size()];
+                temp = new TestData[testsData.size()];
             }
         }
         else {
-            ///xmlMapper.writeValue(this.path, labTest);
-            for (int i = 0; i < labTest.size(); i++) {
-                temp[i] = labTest.get(i);
-            }
-            xmlMapper.writeValue(this.path, temp);
+            ///xmlMapper.writeValue(this.path, testsData);
+//            for (int i = 0; i < testsData.size(); i++) {
+//                temp[i] = testsData.get(i);
+//            }
+//            for (int i = 0; i < testsData.size(); i++) {
+//                //xmlMapper.writeValue(this.path, testsData.get(i).get(0));
+//                //xmlMapper.writeValue(this.path, testsData.get(i).get(0));
+//            }
+            xmlMapper.writeValue(this.path, testsData);
         }
     }
 }
